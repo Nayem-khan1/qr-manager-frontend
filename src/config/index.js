bc8b5dab69a759cad2aad3a28c06beb5046e9e39
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-07-07 20:42:50 +0600

    fix import error

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const backendUrl =
   import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
-export const cloudName = import.meta.env.CLOUDNAME;
-export const uploadPreset = import.meta.env.UPLOADPRESET;
+export const cloudName = import.meta.env.VITE_CLOUDNAME;
+export const uploadPreset = import.meta.env.VITE_UPLOADPRESET;
